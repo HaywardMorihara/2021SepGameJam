302e0ee22053f36f4d06e492cfb8796d6004fdb0
HaywardMorihara <nathanielmorihara@gmail.com>
2021-09-18 12:17:25 -0400

    Small creature sprite and some menu touchups

--- menu/CreatureSelectionMenu.gd
+++ menu/CreatureSelectionMenu.gd
@@ -8,12 +8,15 @@ func _ready():
 	for creature_scene_name in Global.creatures.keys():
 		var creature_selection_option = CREATURE_SELECTION_OPTION.instance()
 		creature_selection_option.init(creature_scene_name)
-		$VBoxContainer.add_child(creature_selection_option)
+		$CreatureSection.add_child(creature_selection_option)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var current_creature_count = 0
+	for creature_info in Global.creatures.values():
+		current_creature_count += creature_info.get("count")
+	$TitleSection/CreaturesLeftLabel.text = "You can pick %s more creatures" % String(Global.MAX_CREATURE_COUNT - current_creature_count)
 
 
 func _on_StartButton_pressed():
